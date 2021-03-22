3966785cf772c7649acdc0e94cae4f1555e65974
Egor Day <edaydev@gmail.com>
2021-03-22 09:15:25 +0200

    Fixed reported desync due to conditional loading

--- data.lua
+++ data.lua
@@ -1,8 +1,4 @@
 require("prototypes.input")
 require("prototypes.shortcut")
 require("prototypes.wire-cutter")
-
-if settings.startup["wire-shortcuts-is-retain-wire-crafting"].value then
-    require("prototypes.fake-item")
-    require("prototypes.fake-recipe")
-end
+require("prototypes.fake-wire")
